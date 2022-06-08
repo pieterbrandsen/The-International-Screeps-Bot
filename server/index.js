62933dc3f201b824db051666a3210415662a07b8
Pieter <pieterbrandsen2@gmail.com>
2022-06-08 22:39:21 +0200

    Test

--- server/index.js
+++ server/index.js
@@ -52,6 +52,8 @@ let botsSpawned = false;
 
 class Tester {
   constructor(length) {
+    console.log(process.env)
+    console.log(process.argv)
     console.log(process.env.STEAM_API_KEY);
     console.log(process.env.STEAM_ID);
     this.roomsSeen = {};
