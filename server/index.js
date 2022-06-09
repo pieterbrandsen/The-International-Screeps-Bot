63367adb221de9a5d1e37f7bcdec1735289c02a9
Pieter <pieterbrandsen2@gmail.com>
2022-06-09 09:14:27 +0200

    Push test

--- server/index.js
+++ server/index.js
@@ -78,7 +78,7 @@ class Tester {
       console.log(`> Start the simulation${appendix}`);
       if (this.maxRuntime > 0) {
         await sleep(this.maxRuntime);
-        // while(lastTick === undefined || lastTick <0.1*1000) continue;
+        while(lastTick === undefined || lastTick < 100);
         console.log(`${lastTick} End of simulation`);
         console.log("Status:");
         console.log(JSON.stringify(status, null, 2));
